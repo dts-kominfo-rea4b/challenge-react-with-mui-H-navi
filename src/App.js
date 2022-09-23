a1113b08d82356eecb19541dd93b97d395a87562
H-navi <hanafinaufal@protonmail.com>
2022-09-23 17:49:08 +0700

    change container/contactApp to app

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-import React from 'react';
-import ContactApp from './containers/ContactApp';
+import { Grid } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+
+import ContactForm from './components/ContactForm';
+import Contact from './components/Contact';
+import Header from './components/Header';
 // Uncomment untuk memuat daftar kontak
 // import contactsJSON from './data/contacts.json';
 const App = () => {
@@ -15,10 +19,28 @@ const App = () => {
 
   // Masukkan contacts yang sudah didapat dalam JSON sebagai initial state
   // Buatlah handler untuk menambahkan kontak baru yang akan dikirim ke ContactForm
+  const [contacts, setContacts] = useState([]);
+
+  const handleClick = (name, phone, email, photo) => {
+    setContacts([...contacts, { nama: name, hp: phone, email: email, foto: photo, id: contacts.length + 1 }]);
+    // setContacts([...phones, { hp: phone, phone_id: phones.length + 1 }]);
+    // setContacts([...emails, { email: email, email_id: emails.length + 1 }]);
+    // setContacts([...photos, { foto: photo, photo_id: photos.length + 1 }]);
+  };
 
   return (
     <div className="App">
-      <ContactApp/>
+      <div className='contact-app'>
+        <Header />
+        <Grid justifyContent="center" container spacing={2}>
+          <Grid item md={6} lg={6}>
+            <ContactForm handleClick={handleClick} />
+          </Grid>
+          <Grid item md={6} lg={6}>
+            <Contact contacts={contacts} />
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 };
